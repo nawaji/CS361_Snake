f3b92db5209a6be4db3fe2faf548128b8f3003a1
nawaji <Jonathant15191@gmail.com>
2022-02-11 17:56:36 -0800

    Basic UI for javascript implementations

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 var path = require('path');
 var express = require('express');
-var exphbs = require("express-handlebars")
+
+var app = express();
+PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//ROUTER
+var router = express.Router();
+
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');  //Import express-handlbars
+app.engine("hbs", engine({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+
+app.use(express.static("public"));
+
+router.get("/", function(req, res, next) {
+
+	res.status(200).render("index");
+});
+
+app.use("/", router);
+
+app.listen(PORT, function() {
+    console.log('Express started on http://local:' + PORT + '; press Ctrl-C to terminate.')
+})
