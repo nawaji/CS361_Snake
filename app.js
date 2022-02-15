69f75228543fc3b3c03e9ec89f8c344e689bba06
nawaji <Jonathant15191@gmail.com>
2022-02-14 20:51:59 -0800

    VERY basic skeleton of the program

--- app.js
+++ app.js
@@ -17,10 +17,46 @@ app.set("view engine", "hbs");
 
 app.use(express.static("public"));
 
+// serve the main web page
 router.get("/", function(req, res, next) {
 
 	res.status(200).render("index");
+
+//	res.status(200).render("index", {
+//		rankings_data
+//	})
+
 });
+
+// when the user adds a listing to the ranks, this is where the
+// teammate's service will be handled. 
+router.post("/add-user-rank", function(req, res, next) {
+
+	// call teammate's service and get the output
+
+	// check if the user already is in the rankings
+
+	// update/add the user in the rankings
+
+	// call teammate's service (IF they allow updating of the .JSON)
+	// to update the file, otherwise do it manually.
+
+});
+
+// update the rankings based on user input in the rankings
+// however, this may be obsolete through the use of javascript
+// interactions within the main page.
+router.post("/update-visible-ranks", function(req, res) {
+
+})
+
+function sortUserRanks() {
+
+}
+
+function filterUserRanks() {
+
+}
 
 app.use("/", router);
 
